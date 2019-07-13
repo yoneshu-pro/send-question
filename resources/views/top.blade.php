61aee8c04c0a452db951b112c5673f07d02ed2bb
yoneshu-pro <r.ta0.p2008@i.softbank.jp>
2019-07-13 19:20:35 +0900

    TOP画面のデザイン作成完了

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,5 +1,60 @@
 @extends('layout.base')
 
 @section('body')
-
+    <div class="row mb-4">
+        <div class="col-md-7">
+            <img src="/img/test.png" class="img-fluid">
+        </div>
+        <div class="col-md-5">
+            <h2>社内の勉強会をやっていく</h2>
+            <p>パワーポイントの共有、質問を匿名で行うことができる</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img class="card-img-top" src="/img/test.png" alt="カードの画像">
+                <div class="card-body">
+                    <h5 class="card-title">カードのタイトル</h5>
+                    <p class="card-text">以下のテキストを追加のコンテンツへの自然な導入としてサポート。 カードのコンテンツ カードのコンテンツ</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img class="card-img-top" src="/img/test.png" alt="カードの画像">
+                <div class="card-body">
+                    <h5 class="card-title">カードのタイトル</h5>
+                    <p class="card-text">以下のテキストを追加のコンテンツへの自然な導入としてサポート。 カードのコンテンツ カードのコンテンツ</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img class="card-img-top" src="/img/test.png" alt="カードの画像">
+                <div class="card-body">
+                    <h5 class="card-title">カードのタイトル</h5>
+                    <p class="card-text">以下のテキストを追加のコンテンツへの自然な導入としてサポート。 カードのコンテンツ カードのコンテンツ</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img class="card-img-top" src="/img/test.png" alt="カードの画像">
+                <div class="card-body">
+                    <h5 class="card-title">カードのタイトル</h5>
+                    <p class="card-text">以下のテキストを追加のコンテンツへの自然な導入としてサポート。 カードのコンテンツ カードのコンテンツ</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card mb-4">
+                <img class="card-img-top" src="/img/test.png" alt="カードの画像">
+                <div class="card-body">
+                    <h5 class="card-title">カードのタイトル</h5>
+                    <p class="card-text">以下のテキストを追加のコンテンツへの自然な導入としてサポート。 カードのコンテンツ カードのコンテンツ</p>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
